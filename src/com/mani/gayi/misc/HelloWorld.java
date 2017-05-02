3602b26fa2386f5b61db9145a478f024dcfba639
MGuduri <manidhar.guduri@gmail.com>
2017-05-02 21:15:33 +0530

    First Commit

---
+++ src/com/mani/gayi/misc/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.mani.gayi.misc;
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Om Nama Sivaya....");
+		System.out.println("Om Gayatri Namaha...");
+	}
+
+}
